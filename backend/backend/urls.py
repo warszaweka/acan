150422552768fd80001bc40980704a3548872c9b
Kyrylo Androsiuk <warszaweka@tuta.io>
2021-12-22 21:10:16 +0200

    Linted formatted

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.contrib.admin import site
 from django.urls import include, path
 from graphene_django.views import GraphQLView
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', site.urls),
-    path('graphql', GraphQLView.as_view(graphiql=True if settings.DEBUG else False)),
+    path('graphql',
+         GraphQLView.as_view(graphiql=True if settings.DEBUG else False)),
     path('acan/', include('acan.urls')),
 ]
